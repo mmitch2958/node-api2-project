1c6ba776f76d7bf424ad10de895317e9832eeb11
mmitch2958 <mikescottmitchell@outlook.com>
2020-09-29 20:20:15 -0400

    changed router name based off assignment req

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = require('./hubs/hubs-router');
-
-//add router from hubs folder 
-const hubsRouter = require('./hubs/hubs-router');
-//5 import router 
-const productsRouter = require('./products/products-router.js')
-
-server.use('/api/posts', hubsRouter);
-
-
+const express = require("express");
 const server = express();
-
 server.use(express.json());
+
+const postsRouter = require("./hubs/postsRouter");
+server.use("/api/posts", postsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h1>idk what this is yet but i'm here hello</h1>`);
+});
+
+server.listen(4000, () => {
+  console.log("\n*** Sevvev running on http://localhost:4000 ***\n");
+});
